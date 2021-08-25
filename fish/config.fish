b8c94dcdf46e81dc67f40e365b466964ae0d44ba
FLDPMpang <frozenpang@gmail.com>
2021-08-25 16:44:00 +0800

    init

---
+++ fish/config.fish
@@ -0,0 +1,35 @@
+set PATH ~/Github/FixVhdWr $PATH
+
+abbr -a ra ranger
+abbr -a nv nvim
+abbr -a lg lazygit
+abbr -a nf "neofetch|lolcat"
+
+
+abbr -a ps " sudo pacman -S"
+abbr -a pss " pacman -Ss"
+abbr -a psi " pacman -Si"
+abbr -a psy " sudo pacman -Sy"
+abbr -a psyyu " sudo pacman -Syyu"
+abbr -a pr " sudo pacman -R"
+abbr -a prs " sudo pacman -Rs"
+abbr -a pq " sudo pacman -Q"
+abbr -a pqi " sudo pacman -Qi"
+abbr -a ys " yay -S"
+abbr -a ysyyu " yay -Syyu"
+abbr -a yss " yay -Ss"
+
+abbr -a cl clear
+abbr -a co code
+
+abbr -a gra " git remote add"
+abbr -a grr " git remote rm"
+abbr -a gre " git remote rename"
+abbr -a gi " git init"
+abbr -a gc " git clone"
+
+abbr -a tn " tmux new -s"
+abbr -a ta " tmux a -t"
+abbr -a tk " tmux kill-session -t"
+abbr -a ts " tmux kill-server"
+abbr -a tl " tmux ls"
